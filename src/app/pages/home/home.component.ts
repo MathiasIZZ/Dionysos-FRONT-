619befe89f75f6b52954d78f81d0d5f43f4e56d8
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2022-04-01 13:49:56 +0200

    update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  urlLogo="../../../assets/images/logo.jpg";
+
+  constructor() {
+
+   }
 
   ngOnInit(): void {
   }
