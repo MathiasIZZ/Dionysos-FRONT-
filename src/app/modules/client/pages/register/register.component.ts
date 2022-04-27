34d40137fc4612215cb4d74877e08023cac35934
ElNaj <nelfateoui@gmail.com>
2022-04-27 15:39:24 +0200

    DIONYSOS-28_FRONT-Permettre enregistrement/inscription + modification login - contrôle saisiutilisateur

--- src/app/modules/client/pages/register/register.component.ts
+++ src/app/modules/client/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { TokenStorageService } from 'src/app/auth/token-storage.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    username: null,
+    email: null,
+    password: null
+  };
+  isSignup = false;
+  isSignupFailed = false;
+  errorMessage = '';
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void { }
+
+  onSubmit(): void {
+    const { username, email, password } = this.form;
+    this.authService.register(username, email, password).subscribe({
+      next: (data) => {
+        console.log("data : " + data);
+        this.isSignup = true;
+        this.isSignupFailed = false;
+      },
+      error: (err) => {
+        this.errorMessage = err.error.message;
+        this.isSignupFailed  = true;
+      }
+    });
+  }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
+
+  goToHome() {
+    window.location.assign("");
   }
 
 }
