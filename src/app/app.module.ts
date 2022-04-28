69a56fecfd07d16f500f360c5fbec46929d12868
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2022-04-28 15:16:46 +0200

    update conflit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
-<<<<<<< HEAD
+
 import { NotfoundComponent } from './pages/notfound/notfound.component';
-=======
 import { authInterceptorProviders } from './helpers/auth.interceptor';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './modules/client/pages/login/login.component';
 import { RegisterComponent } from './modules/client/pages/register/register.component';
 import { UserComponent } from './modules/client/pages/user/user.component';
->>>>>>> develop
+
 
 
 @NgModule({
